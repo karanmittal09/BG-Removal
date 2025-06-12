e779049042fcdbb9fcb1fad509a3316dca1fe36b
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-06-12 15:43:58 +0530

    Implement Header, Steps, BgSlider, and Testimonials components; update Home page to include new components and enhance styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
+import Steps from '../components/Steps'
+import BgSlider from '../components/BgSlider'
+import Testimonials from '../components/Testimonials'
+
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Steps/>
+      <BgSlider/>
+      <Testimonials/>
     </div>
   )
 }
