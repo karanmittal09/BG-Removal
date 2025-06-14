bcb09ce1de3ba41ce4c2a44c9ea1463a7e3017e8
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-06-14 23:42:53 +0530

    backend initialized

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets';
 import { Link } from 'react-router-dom';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
+  
+  const { openSignIn} = useClerk();
+  const {isSignedIn, user} = useUser()
+
   return (
     <div className='flex items-center justify-between px-4 py-3 lg:mx-44'>
       
@@ -10,9 +15,18 @@ const Navbar = () => {
       <img className='w-32 sm:w-44' src={assets.logo} alt="" />
       </Link>
 
-      <button className='bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 rounded-lg hover:bg-zinc-700 transition-colors duration-300'>
+      {
+        isSignedIn 
+        ?<div>
+          <UserButton/>
+        </div>
+        : <button onClick={()=>openSignIn({})} className='bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 rounded-full hover:bg-zinc-700 transition-colors duration-300'>
         Get Started <img className='w-3 sm:w-4' src={assets.arrow_icon} alt="" />
         </button>
+
+      }
+
+      
     </div>
   )
 }
