9eb84a212a771e599c90920b7b269281a59587a2
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-06-15 16:59:48 +0530

    Set up server with MongoDB connection; add user model and controller; configure environment variables

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-import 'dotenv/config'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/mongodb.js';
+
+//App configs
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+await connectDB(); 
+
+
+// Initialize Middlewares
+app.use(express.json());
+app.use(cors());
+
+//Api routes
+
+app.get('/', (req, res) => res.send("API Working"));
+
+app.listen(PORT, () => console.log("Server Running on port " + PORT));
