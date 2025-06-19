c12541f079d68d109d4ef5cd81498bbb5e89dab3
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-06-19 15:14:14 +0530

    Add Clerk webhook handling to UserController and update .env configuration

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,49 @@
-
+import {WebHook} from 'svix'
 
 const clerkWebhooks = async(req, res) => {
+
+  try{
+
+    const whook = new WebHook(process.env.CLERK_WEBHOOK_SECRET);
+
+    await whook.verify(JSON.stringify(req.body),{
+        "svix-id" : req.headers['svix-id'],
+        "svix-timestamp" : req.headers['svix-timestamp'],
+        "svix-signature" : req.headers['svix-signature'],
+    });
+
+    const {data, type} = req.body;
+
+    switch(type){
+       case "user.created": {
+
+
+          break;
+       }
+
+       case "user.updated": {
+
+
+          break;
+       }
+
+
+       case "user.deleted": {
+
+
+          break;
+       }     
+
+
+       default:
+          break;
+    }
+
+  }
+  catch(error){
+    console.log(error.message);
+    res.json({success: false, message: error.message});
+  }
+
 
 }
